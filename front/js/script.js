6b0cb9d072ba5f8a67928ee7acae020116132b8e
jpdagnano <jeanp.dagnano@gmail.com>
2022-03-30 22:08:49 +0200

    creation trame js/htms

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,14 @@
-let dataApi = fetch("http://localhost:3000/api/products");
-dataApi.then((response) => {
-  const kanapData = response.json();
-  console.log(kanapData);
+let eltItem = document.getElementById("items");
+let itemA = document.createElement("a");
+let articleItem = document.createElement("article");
+let imgItem = document.createElement("img");
+let nameItem = document.createElement("h3");
+nameItem.setAttribute("class", "productName");
+let descItem = document.createElement("p");
+descItem.setAttribute("class", "productDescription");
 
-  kanapData.then((fullKanap) => {
-    const nomKanap = fullKanap[0].name;
-    console.log(nomKanap);
-    let nomProduit = document.querySelector(".productName");
-    nomProduit.innerHTML = nomKanap;
-  });
-});
+eltItem.appendChild(itemA);
+itemA.appendChild(articleItem);
+articleItem.appendChild(imgItem);
+articleItem.appendChild(nameItem);
+articleItem.appendChild(descItem);

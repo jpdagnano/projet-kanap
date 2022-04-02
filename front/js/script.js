ddfd9f6a93c1a4e16326f1e848842201bc3c5fb3
jpdagnano <jeanp.dagnano@gmail.com>
2022-04-02 21:28:20 +0200

    ajout dynamique des objets

--- front/js/script.js
+++ front/js/script.js
@@ -1,28 +1,32 @@
-let eltItem = document.getElementById("items");
-let itemA = document.createElement("a");
-let articleItem = document.createElement("article");
-let imgItem = document.createElement("img");
-let nameItem = document.createElement("h3");
-nameItem.setAttribute("class", "productName");
-let descItem = document.createElement("p");
-descItem.setAttribute("class", "productDescription");
-
-eltItem.appendChild(itemA);
-itemA.appendChild(articleItem);
-articleItem.appendChild(imgItem);
-articleItem.appendChild(nameItem);
-articleItem.appendChild(descItem);
-
 fetch("http://localhost:3000/api/products").then((response) =>
   response.json().then((data) => {
     console.log(data);
     data.forEach(function (donneesKanap) {
-      console.log(donneesKanap.name);
-      for (let i = 0; i < donneesKanap.name; i++) {
-        document.createElement(eltItem);
-        console.log(i);
-      }
+      let eltItem = document.getElementById("items");
+      let itemA = document.createElement("a");
+      let articleItem = document.createElement("article");
+      let imgItem = document.createElement("img");
+
+      let nameItem = document.createElement("h3");
+      nameItem.setAttribute("class", "productName");
+      let descItem = document.createElement("p");
+      descItem.setAttribute("class", "productDescription");
+
+      eltItem.appendChild(itemA);
+      itemA.appendChild(articleItem);
+      articleItem.appendChild(imgItem);
+      articleItem.appendChild(nameItem);
+      articleItem.appendChild(descItem);
+      let i = 0;
+      i < donneesKanap;
+      i++;
+
       nameItem.innerHTML = donneesKanap.name;
+      descItem.innerHTML = donneesKanap.description;
+      imgItem.src = donneesKanap.imageUrl;
+      imgItem.alt = donneesKanap.altTxt;
+
+      console.log(i);
     });
   })
 );

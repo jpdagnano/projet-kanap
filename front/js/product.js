95406e000f4491ce1c1b03bbdbbd102c9036b6e6
jpdagnano <jeanp.dagnano@gmail.com>
2022-04-06 23:55:26 +0200

    insertion item sur page pdt

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,22 @@
 const pageIdWindow = window.location.search;
 
-const pageIdLong = new URLSearchParams(pageIdWindow);
-const pageId = pageIdLong.get("_id");
+const longId = new URLSearchParams(pageIdWindow);
+const pageId = longId.get("_id");
+console.log(pageId);
+
+fetch("http://localhost:3000/api/products/" + pageId).then((response) =>
+  response.json().then((data) => {
+    console.log(data);
+    let price = document.getElementById("price");
+    price.innerHTML = data.price;
+
+    let description = document.getElementById("description");
+    description.innerHTML = data.description;
+
+    let imgItem = document.createElement("img");
+    let divImg = document.querySelector(".item__img");
+    divImg.appendChild(imgItem);
+    imgItem.src = data.imageUrl;
+    imgItem.alt = data.altTxt;
+  })
+);

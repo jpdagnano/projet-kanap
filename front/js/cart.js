57e383ef7b180679af1f6870e83a7b5fb2195fb3
jpdagnano <jeanp.dagnano@gmail.com>
2022-05-02 21:48:04 +0200

    panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,24 +1,21 @@
-let donneesKanap = localStorage.getItem("keyKanap");
-let donneesParse = JSON.parse(donneesKanap);
+let donneesParse = JSON.parse(localStorage.getItem("keyKanap"));
+
 console.log(donneesParse);
 
-donneesParse.forEach((objets) => {
+donneesParse.forEach(() => {
   let template = document.querySelector("template");
-  console.log(donneesParse);
 
   let copieTemplate = document.importNode(template.content, true);
   let blocContent = document.getElementById("cart__items");
   blocContent.appendChild(copieTemplate);
   const article = document.querySelector(".cart__item");
-  console.log(article);
-  let artDataset = donneesParse[0].id;
+
+  let i = 0;
+  let artDataset = donneesParse[i].id;
   fetch("http://localhost:3000/api/products/" + artDataset).then((response) =>
     response.json().then((data) => {
       console.log(data);
-      let i = 0;
-      i < donneesParse;
-      artDataset++;
-      i++;
+   
     })
   );
 });

2386e1115874aea88a601e9b595aeae49d13e70d
jpdagnano <jeanp.dagnano@gmail.com>
2022-05-21 22:42:04 +0200

    fin crt

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,12 +6,12 @@ let sommeTotal = 0;
 function delItem(elem) {
   Element.remove();
 }
-for (let i = 0; i < donneesParse.length; i++) {
+for (let i = 0; i < itemCanap.length; i++) {
   const copieTemplate = document.importNode(template.content, true);
   //const copieTemplate = template.content.cloneNode(true);
   const blocContent = document.getElementById("cart__items");
   let divDelete = copieTemplate.querySelector(".cart__item");
-  fetch("http://localhost:3000/api/products/" + donneesParse[i].id).then(
+  fetch("http://localhost:3000/api/products/" + itemCanap[i].id).then(
     (response) =>
       response.json().then((data) => {
         const imagePdt = copieTemplate.children[0].querySelector("img");
@@ -33,18 +33,18 @@ for (let i = 0; i < donneesParse.length; i++) {
 
         const article = copieTemplate.querySelector(".cart__item");
         article.dataset.dataId = data._id;
-        article.dataset.dataColor = donneesParse[i].color;
+        article.dataset.dataColor = itemCanap[i].color;
 
-        let quantityNumber = parseInt(donneesParse[i].quantite);
+        let quantityNumber = parseInt(itemCanap[i].quantite);
         imagePdt.src = data.imageUrl;
         nomCanap.innerHTML = data.name;
-        couleurCanap.innerHTML = donneesParse[i].color;
+        couleurCanap.innerHTML = itemCanap[i].color;
         priceCanap.innerHTML = data.price + " €";
         quantityCanap.value = quantityNumber;
-        totalQuantity.innerHTML = donneesParse.length;
+        totalQuantity.innerHTML = itemCanap.length;
         sommeTotal += data.price * quantityNumber;
         totalPrice.innerHTML = sommeTotal;
-        console.log(donneesParse.length);
+        console.log(itemCanap.length);
 
         //modification quantité dans le local storage si modifié sur page panier
         quantityCanap.addEventListener("change", modifQuantite);
@@ -55,18 +55,17 @@ for (let i = 0; i < donneesParse.length; i++) {
           window.location.reload();
         }
         deleteButton.addEventListener("click", () => {
-          if (donneesParse.length > 1) {
+          if (itemCanap.length > 1) {
+            let index = itemCanap.indexOf(i);
+            console.log(index);
             divDelete.parentNode.removeChild(divDelete);
-            itemCanap.splice(1);
+            itemCanap.splice(index, 1);
             localStorage.setItem("keyKanap", JSON.stringify(itemCanap));
             itemCanap = JSON.parse(localStorage.getItem("keyKanap"));
-            console.log("cas1");
             window.location.reload();
-            console.log(donneesParse);
           } else {
             divDelete.parentNode.removeChild(divDelete);
             localStorage.removeItem("keyKanap");
-            window.location.reload();
           }
 
           // si positionnement de la ligne imagePdt après la ligne blocContent.appendChild ALORS ERREUR
